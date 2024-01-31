656cafd730e8145e3abea6a410de8b0a00748d7c
zachpfaltzgraff <141784641+zachpfaltzgraff@users.noreply.github.com>
2024-01-31 17:41:29 -0500

    Created
    Created

---
+++ src/HexConverterClient.java
@@ -0,0 +1,30 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class HexConverterClient {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 12345);
+            while (true) {
+
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter a binary string: ");
+                String binaryString = scanner.nextLine().trim();
+
+                System.out.println("Sending binary string to server: " + binaryString);
+                output.writeUTF(binaryString);
+
+                String hexString = input.readUTF();
+                System.out.println("Received hexadecimal string from server: " + hexString);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/HexConverterServer.java
@@ -0,0 +1,43 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HexConverterServer {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("Server started. Waiting for clients...");
+
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress());
+
+            while (true) {
+                DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+
+                String binaryString = input.readUTF();
+                System.out.println("Binary Value Recieved: " + binaryString);
+                String hexString = convertBinaryToHex(binaryString);
+                System.out.println("Hex Value Sent: " + hexString);
+
+                output.writeUTF(hexString);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String convertBinaryToHex(String binaryString) {
+        int decimalValue = Integer.parseInt(binaryString, 2);
+        StringBuilder hexString = new StringBuilder(Integer.toHexString(decimalValue));
+
+        while (hexString.length() < 2) {
+            hexString.insert(0, "0");
+        }
+
+        return hexString.toString();
+    }
+}
